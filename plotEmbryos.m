994ed0e751ac250c9ec776b2e41a1e68070d28f8
lsteve8 <lsteve8@clemson.edu>
2026-06-23 16:51:02 +0900

    Addition of plotting functions

---
+++ plotEmbryos.m
@@ -0,0 +1,59 @@
+function plotEmbryos(workspace,embryos)
+
+% draw 2D plot for work function
+figure;
+hold on;   
+
+% workspace size
+width = workspace.size(1);
+depth = workspace.size(2);
+
+% draw boundary
+rectangle('Position', workspace.activeregion,'Edgecolor','w', 'Linewidth',2)
+
+% plot each embryo 
+for i = 1:length(embryos)
+    % create varying orientation for embryos
+   theta = linspace(0, 2*pi, 50);
+
+    xCenter = embryos(i).position(1);
+   yCenter = embryos(i).position(2);
+
+   a = embryos(i).length/2;
+   b = embryos(i).width/2;
+
+   xEllipse = a*cos(theta);
+   yEllipse = b*sin(theta);
+
+   ellipsePoints = [xEllipse; yEllipse; zeros(size(theta))];
+
+   R = embryos(i).orientation;
+    % add arrows to see intended orientation in the process
+   direction = R(:,1);
+   
+   arrowlength = embryos(i).length;
+
+   quiver(xCenter, yCenter, arrowlength*direction(1),arrowlength*direction(2),0,'Linewidth',1.5,'MaxHeadSize',2)
+
+   rotatedPoints = R* ellipsePoints;
+% plot the embryos with varying orientation as ellipses onto the workspace
+   
+
+   xPlot = rotatedPoints(1,:) + xCenter;
+   yPlot = rotatedPoints(2,:) + yCenter;
+  
+
+   plot(xPlot, yPlot,'cyan','Linewidth',1)
+    text(xCenter+0.2, yCenter+0.2,num2str(i),'Fontsize',8,'FontWeight','bold')
+end
+
+axis equal;
+xlim([0 width]);
+ylim([0 depth]);
+
+xlabel('X axis');
+ylabel('Y axis');
+title('2D Visualization');
+
+grid on;
+end
---
+++ plotEmbryos3D.m
@@ -0,0 +1,54 @@
+function plotEmbryos3D(workspace,embryos,showIDs,showArrows)
+
+% translate the plotting into a 3D version
+
+figure; 
+hold on;
+
+% workspace size
+width = workspace.size(1);
+height = workspace.size(2);
+depth = workspace.size(3);
+
+
+% workspace region
+%rectangle('Position',width,height,depth,'EdgeColor','w','Linewidth',2)
+
+% plot each of the embryo in 3D
+for i = 1:length(embryos)
+    
+   [xEll, yEll, zEll] = ellipsoid(0, 0, 0, embryos(i).length/2, embryos(i).width/2, embryos(i).height/2, 10);
+   points = [xEll(:)'; yEll(:)'; zEll(:)'];
+
+   R = embryos(i).orientation;
+   rotatedPoints = R*points;
+
+  xPlot = reshape(rotatedPoints(1,:), size(xEll)) + embryos(i).position(1);
+  yPlot = reshape(rotatedPoints(2,:), size(yEll)) + embryos(i).position(2);
+  zPlot = reshape(rotatedPoints(3,:), size(zEll)) + embryos(i).position(3);
+
+  surf(xPlot,yPlot,zPlot)
+  if showIDs
+  text(embryos(i).position(1)+0.2,embryos(i).position(2)+0.2,embryos(i).position(3)+0.2,num2str(i),'FontSize',8,'FontWeight','bold')
+  end
+  if showArrows
+      direction = R(:,1);
+   
+   arrowlength = embryos(i).length;
+
+   quiver3(embryos(i).position(1), embryos(i).position(2),embryos(i).position(3), arrowlength*direction(1),arrowlength*direction(2),0,'Linewidth',1.5,'MaxHeadSize',2)
+  end
+end
+axis equal;
+xlim([0 width]);
+ylim([0 height]);
+zlim([0 depth]);
+
+xlabel('X axis')
+ylabel('Y axis')
+zlabel('Z axis')
+title('3D visualization')
+
+view(3)
+grid on;
+end
